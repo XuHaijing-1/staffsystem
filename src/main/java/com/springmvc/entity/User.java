44a495618416a3f1b9f8ab579e39319f68567b8a
Xuhaijing <63711186+XuHaijing-1@users.noreply.github.com>
2020-07-01 19:26:14 +0800

    创建实体类

--- src/main/java/com/springmvc/entity/User.java
+++ src/main/java/com/springmvc/entity/User.java
@@ -1,18 +1,13 @@
 package com.springmvc.entity;
 
-import java.io.Serializable;
-import org.springframework.data.annotation.Id;
-import com.fasterxml.jackson.annotation.JsonFormat;
-
 /**
  * 用户模型类
  * @author 憨态可拘
  *
  */
-public class User implements Serializable{
+public class User{
 	private static final long serialVersionUID = 8245746230705180748L;
 	public static final String CACHE_NAME="users";
-	@Id
 	private String id;  //用户id
 	private String year; //用户年龄
 	private String username;  //用户名
